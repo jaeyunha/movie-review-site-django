951577ee3fa65214b6115695375949906e4b172d
jaeyunha <jaeyunha@berkeley.edu>
2022-08-02 17:19:24 -0700

    after completing backend

--- movieapp/views.py
+++ movieapp/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+import requests
+import json
+from .forms import SearchForm
 
-# Create your views here.
+my_id = '24f7c99927298a37d37cd8fd31b1bdfd'
+def home(request):
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        searchword = request.POST.get('search')
+        if form.is_valid():
+            url = 'https://api.themoviedb.org/3/search/movie?api_key='+my_id+'&query='+searchword
+            response = requests.get(url)
+            resdata = response.text
+            obj = json.loads(resdata)
+            obj = obj['results']
+            return render(request, 'search.html', {'obj':obj})
+        # https://api.themoviedb.org/3/search/movie?
+        # api_key=24f7c99927298a37d37cd8fd31b1bdfd&language=en-US&query=hello&page=1&include_adult=false
+        # 위 형태의 url로 get 요청 보내기
+    else:
+        form = SearchForm()
+        url = 'https://api.themoviedb.org/3/trending/all/day?api_key='+my_id
+        response = requests.get(url)
+        resdata = response.text
+        obj = json.loads(resdata)
+        obj = obj['results']
+
+    return render(request, 'index.html', {'obj':obj, 'form':form})
+
+def detail(request, movie_id):
+
+    url = 'https://api.themoviedb.org/3/movie/'+ movie_id + 'api_key='+my_id
+    response = requests.get(url)
+    resdata = response.text
+    return render(request, 'detial.html', {"redata": resdata})
+
+    return
--- movieproj/settings.py
+++ movieproj/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = mySECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'movieapp',
 ]
 
 MIDDLEWARE = [
@@ -99,6 +100,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
